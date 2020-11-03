71a3fa0571364ada366548fec8849b4d84790792
BRIAN BLAZE <brian.blaze@ust-global.com>
2020-11-03 20:32:31 +0530

    changes in the security layer

---
+++ src/main/java/com/hello/hellotester/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.hello.hellotester;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(STATELESS);
+        http.headers().frameOptions().disable()
+                .xssProtection().block(false);
+    }
+
+
+}
