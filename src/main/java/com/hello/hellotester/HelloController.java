df79480114613af5cb5cff780477b007f52c6dee
BRIAN BLAZE <brian.blaze@ust-global.com>
2020-11-03 20:20:22 +0530

    addition of dependencies

--- src/main/java/com/hello/hellotester/HelloController.java
+++ src/main/java/com/hello/hellotester/HelloController.java
@@ -1,5 +1,6 @@
 package com.hello.hellotester;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +9,17 @@ import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 
 @RestController
+@Slf4j
 public class HelloController {
     @GetMapping(value = "/")
     public String sayHello() {
+        log.info("Inside the hello1");
         return "HELLO-USER " + LocalDateTime.now();
     }
 
     @GetMapping(value = "/hello")
     public String sayHelloUser() throws UnknownHostException {
+        log.info("Inside the hello2");
         return "HELLO-USER " + LocalDateTime.now() + " " + InetAddress.getLocalHost().getHostName();
     }
 }
